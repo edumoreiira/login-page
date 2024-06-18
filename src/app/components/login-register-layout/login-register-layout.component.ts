491df0a64628802c0427066886585b9a2ae7be4c
Eduardo Moreira <dudu_moreira9@hotmail.com>
2024-06-18 20:52:44 -0300

    create input component

--- src/app/components/login-register-layout/login-register-layout.component.ts
+++ src/app/components/login-register-layout/login-register-layout.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation, input } from '@angular/core';
 
 @Component({
   selector: 'app-login-register-layout',
   standalone: true,
   imports: [],
   templateUrl: './login-register-layout.component.html',
-  styleUrl: './login-register-layout.component.scss'
+  styleUrl: './login-register-layout.component.scss',
 })
 export class LoginRegisterLayoutComponent {
+  @Input() pageTitle: string = "";
+  @Output("submit") onSubmit = new EventEmitter;
 
+
+
+
+
+  submit(){
+    this.onSubmit.emit();
+  }
 }
--- src/app/pages/login-page/login/login.component.ts
+++ src/app/pages/login-page/login/login.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { LoginRegisterLayoutComponent } from '../../../components/login-register-layout/login-register-layout.component';
+import { FormInputComponent } from '../../../components/form-input/form-input.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginForm} from '../../../models/login.interface';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginRegisterLayoutComponent],
+  imports: [LoginRegisterLayoutComponent, FormInputComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginForm!: FormGroup<LoginForm>;
+
+  constructor(){
+    
+    
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    })
+  }
+
+  submit(): void{
+    console.log(this.loginForm.value)
+  }
 }
